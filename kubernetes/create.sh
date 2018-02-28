c20f8846e38acbadd00a2defd590d260d4d90d27
Andrei Mihaescu <amihaescu@deloittece.com>
2018-02-28 18:10:01 +0200

    finalized create/destroy scripts

--- kubernetes/create.sh
+++ kubernetes/create.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+kubernetes create -f backend-deployment.yaml
+kubernetes create -f backend-service.yaml
+kubernetes create -f frontend-deployment.yaml
+kubernetes create -f frontend-service.yaml
--- kubernetes/destroy.sh
+++ kubernetes/destroy.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+kubectl delete service beers-app-service
+kubectl delete service beers-app-fe-service
+kubectl delete deployment beers-back-end-deployment
+kubectl delete deployment beers-front-end-deployment
